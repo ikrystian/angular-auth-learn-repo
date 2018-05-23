3ffa445b524fdc1dade2a1f7ed702bdb53b040fe
Tomasz Syryczyk <tomasz.syryczyk@gmail.com>
2018-05-23 23:29:05 +0200

    most renames, small fixes, introduction to views/services file management

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { EventsComponent } from './events/events.component';
-import { SpecialEventsComponent } from './special-events/special-events.component';
-import { AuthService } from './auth.service';
-import { EventService } from './event.service';
-import { AuthGuard } from './auth.guard';
-import { TokenInterceptorService} from './token-interceptor.service';
+import { RegisterComponent } from './views/register/register.component';
+import { LoginComponent } from './views/login/login.component';
+import { EventsComponent } from './views/events/events.component';
+import { SpecialEventsComponent } from './views/special-events/special-events.component';
+import { AuthService } from './services/authorization/auth.service';
+import { EventService } from './services/event/event.service';
+import { AuthGuard } from './services/authorization/auth.guard';
+import { TokenInterceptorService} from './services/token-interceptor/token-interceptor.service';
 
 @NgModule({
   declarations: [
