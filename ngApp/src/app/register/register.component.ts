5d904c0b67448e8876ba7cf67c767964bbd6672b
Krystian <kkuzminski@betacom.com.pl>
2018-05-23 22:29:51 +0200

    Redirecting after login or register

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -9,7 +10,10 @@ import { AuthService } from '../auth.service';
 export class RegisterComponent implements OnInit {
 
   registerUserData = {};
-  constructor(private _auth: AuthService) { }
+  constructor(
+    private _auth: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -17,7 +21,10 @@ export class RegisterComponent implements OnInit {
   registerUser() {
     this._auth.registerUser(this.registerUserData)
       .subscribe(
-        res => console.log(res),
+        res => {
+          localStorage.setItem('token', res.token);
+          this._router.navigate(['/special']);
+        },
         err => console.log(err)
       )
   }
