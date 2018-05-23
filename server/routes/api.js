7f5b79ddf14dc40c26aeb5ef986aa9faabf0755d
Krystian <kkuzminski@betacom.com.pl>
2018-05-23 19:32:10 +0200

    Add simple registration api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const User = require('../models/user');
 const mongoose = require('mongoose');
 // yup - credentials to database in GH repo <3
 const db = "mongodb://user:password@ds231559.mlab.com:31559/ngdb";
@@ -15,6 +15,18 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
     res.send('From API route');
+});
+
+router.post('/register', (req, res) => {
+    let userData = req.body;
+    let user = new User(userData);
+    user.save((error, registerUser) => {
+        if(error) {
+            console.log(error);
+        } else {
+            res.status(200).send(registerUser);
+        }
+    });
 });
 
 module.exports = router;
