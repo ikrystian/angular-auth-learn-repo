eaf02c53392b634ad789e1ebfda4e510546c3b69
Krystian <kkuzminski@betacom.com.pl>
2018-05-23 19:28:55 +0200

    Add mongodb connect

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+// yup - credentials to database in GH repo <3
+const db = "mongodb://user:password@ds231559.mlab.com:31559/ngdb";
+
+mongoose.connect(db, err => {
+    if(err) {
+        console.log('Error: ' + err);
+    } else {
+        console.log('Connected to mongodb');
+    }
+});
+
 router.get('/', (req, res) => {
     res.send('From API route');
 });
